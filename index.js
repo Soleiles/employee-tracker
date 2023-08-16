f40c5479f4a0002b7be9a20a4e37057cf73823e5
Soleiles <ktaing789@gmail.com>
2023-08-16 03:52:07 -0400

    installed packages and added seeds

---
+++ index.js
@@ -0,0 +1,29 @@
+const inquirer = require('inquirer');
+const { table } = require('table');
+const mysql = require('mysql12/promise');
+const figlet = require('figlet');
+
+let db;
+
+const config = {
+  border: {
+    topBody: `─`,
+    topJoin: `┬`,
+    topLeft: `┌`,
+    topRight: `┐`,
+
+    bottomBody: `─`,
+    bottomJoin: `┴`,
+    bottomLeft: `└`,
+    bottomRight: `┘`,
+
+    bodyLeft: `│`,
+    bodyRight: `│`,
+    bodyJoin: `│`,
+
+    joinBody: `─`,
+    joinLeft: `├`,
+    joinRight: `┤`,
+    joinJoin: `┼`
+  }
+};
