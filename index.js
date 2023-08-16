c74cec648c73a438a6b331a719e4f4cdff66e7d8
Soleiles <ktaing789@gmail.com>
2023-08-16 04:31:38 -0400

    connection to sql database;

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const { table } = require('table');
-const mysql = require('mysql12/promise');
+const mysql = require('mysql2/promise');
 const figlet = require('figlet');
 
 let db;
@@ -27,3 +27,31 @@ const config = {
     joinJoin: `┼`
   }
 };
+
+mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'hr_db'
+    }
+).then((connection) => {
+    db = connection;
+    console.log('Successfully connected to hr_db');
+    // init();
+});
+
+async function showTable(data) {
+    let table = [];
+    tableData = [
+        Object.keys(data[0]), 
+        ...data.map(val => Object.values(val))
+    ];
+    const answers = await inquirer.createPromptModule([
+        {
+            message: "\n" + table(tableData, config),
+            type: "input",
+            name: 'name'
+        }
+    ]);
+}
